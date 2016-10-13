faa9ef2ea6d8d833aa321f249c7a0d27153afbcd
Mike Voermans <mike.voermans@hotmail.nl>
2016-10-13 11:06:26 +0200

    Added content views in all activitys.

--- app/src/main/java/com/photofactory/photofactorygame/UploadActivity.java
+++ app/src/main/java/com/photofactory/photofactorygame/UploadActivity.java
@@ -11,6 +11,7 @@ public class UploadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
     }
 }
