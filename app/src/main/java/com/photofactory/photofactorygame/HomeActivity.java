95bdd572f58ba9fbb7f37741616fd8b8a32a589d
Rens Philipsen <philipsen.rens@gmail.com>
2016-10-20 10:14:48 +0200

    Added HomeActivity functionality

--- app/src/main/java/com/photofactory/photofactorygame/HomeActivity.java
+++ app/src/main/java/com/photofactory/photofactorygame/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.photofactory.photofactorygame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TableRow;
 
 /**
  * Created by Mike on 13-Oct-16.
@@ -13,5 +16,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TableRow tableRow = (TableRow) findViewById(R.id.tableRow);
+
+        tableRow.setClickable(true);
+        tableRow.setOnClickListener(onClickListener);
     }
+
+    private View.OnClickListener onClickListener= new View.OnClickListener() {
+        public void onClick(View v) {
+            startActivity(new Intent(HomeActivity.this, ContestActivity.class));
+        }
+    };
 }
--- app/src/main/java/com/photofactory/photofactorygame/LoginActivity.java
+++ app/src/main/java/com/photofactory/photofactorygame/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.photofactory.photofactorygame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Mike on 13-Oct-16.
@@ -13,5 +16,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn = (Button)findViewById(R.id.btnLogin);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        });
     }
 }
